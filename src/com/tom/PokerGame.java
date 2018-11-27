3ccae6ee148906c9e2a205569ef1dc7a06f26de3
Bam <wubetty0727@gmail.com>
2018-11-27 11:54:13 +0800

    second

--- src/com/tom/PokerGame.java
+++ src/com/tom/PokerGame.java
@@ -6,9 +6,39 @@ public class PokerGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] flowers = {0x2663, 0x2665, 0x2660, 0x2666};
+		int[] nums = new int[5];
+		nums[0] = 76;
+		nums[4] = 98;
+		System.out.println(nums[0]);
+		System.out.println(nums.length);
+		for(int i = 0; i<nums.length; i++){
+			System.out.println(nums[i]);
+		}
+		char f = 0x2663;
+		System.out.println(f);
+		
 		Random random = new Random();
-		int card = random.nextInt((13)+1);
-		int a = card/4;
+		int card = random.nextInt(13)+1;
+		int flower = random.nextInt(4);
+		System.out.print(card);
+		char c = 0;
+		switch(flower){
+		case 0:
+			c = 'C';
+			break;
+		case 1:
+			c = 'H';
+			break;
+		case 2:
+			c = 'S';
+			break;
+		default:
+			c = 'D';
+			break;
+		}
+		//System.out.println(c);
+		/*int a = card/4;
 		System.out.print(card);
 		if(a>=3){
 			System.out.println("H");
@@ -18,26 +48,9 @@ public class PokerGame {
 			System.out.println("D");
 		}else{
 			System.out.println("C");
-		}
-
-		/*public char color(){
-			char colorr = 'C';
-			switch(card/4){
-			case 4:
-				colorr = 'H';
-				break;
-			case 3:
-				colorr = 'C';
-				break;
-			case 2:
-				colorr = 'D';
-				break;
-			default:
-				colorr = 'S';
-				break;
-			}
-			return colorr;
 		}*/
+
+		
 	}
 
 }
